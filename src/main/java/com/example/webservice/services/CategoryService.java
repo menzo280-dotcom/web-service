8036bfeea8caac3c3e80ad704cdef24b69161ad1
Enzo Martins <menzo280@gmail.com>
2026-05-19 18:13:07 -0300

    Category

---
+++ src/main/java/com/example/webservice/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.example.webservice.services;
+
+import java.util.List;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.webservice.entities.Category;
+import com.example.webservice.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long Id) {
+		Optional <Category> obj = repository.findById(Id);
+		return obj.get();
+	}
+
+}
