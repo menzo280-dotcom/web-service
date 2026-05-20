e019196621f86ee9f8c4ab9d98e5ea4d7346b6fa
Enzo Martins <menzo280@gmail.com>
2026-05-20 11:27:18 -0300

    Subtotal & Total methods

--- src/main/java/com/example/webservice/entities/OrderItem.java
+++ src/main/java/com/example/webservice/entities/OrderItem.java
@@ -39,7 +39,7 @@ public class OrderItem implements Serializable{
 		id.setOrder(order);
 		
 	}
-
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable{
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
